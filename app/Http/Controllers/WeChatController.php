d308f3507e418c5831610512d562b5d10b58bdc4
游天琪 <autumnyout@gmail.com>
2018-07-02 12:03:02 +0800

    test

--- app/Http/Controllers/WeChatController.php
+++ app/Http/Controllers/WeChatController.php
@@ -14,22 +14,17 @@ class WeChatController extends Controller
   public function serve()
   {
     Log::info('request arrived.'); # 注意：Log 为 Laravel 组件，所以它记的日志去 Laravel 日志看，而不是 EasyWeChat 日志
+    $officialAccount = EasyWeChat::officialAccount();
 
-    $app = app('wechat.official_account');
-    return $app->template_message->send([
-      'touser' => 'gh_1ccb4d091daa',
-      'template_id' => 'PKgrrNma6Z_6zcCGYde9d7DD6-fFBFGc9sIMChFWdvs',
-      'url' => 'http://baidu.com',
-      'data' => [
-        'first' => '你好',
-        'name' => '浦发银行',
-        'target' => '10',
-        'latest' => '10.5',
-        'time' => '2018-07-02 10:08:22',
-        'remark' => '优财助手~',
-        ],
-    ]);
+    $officialAccount->server->push(function ($message) {
+      return "您好！欢迎使用 EasyWeChat!";
+    });
 
-//    return $app->server->serve();
+//    $response = $officialAccount->server->serve();
+
+
+    Log::info(json_encode($officialAccount));
+
+    return $officialAccount->server->serve();
   }
 }
