82a91dd090a57a74513b6c3fa0f6cb32362c918c
twohappy <autumnyout@gmail.com>
2018-07-02 10:25:14 +0800

    test

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -1,13 +1,5 @@
-<?php
+<?php namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
-
-/**
- * Created by PhpStorm.
- * User: twohappy
- * Date: 2018/7/2
- * Time: 上午10:23
- */
 class TestController extends Controller
 {
   public function index()
